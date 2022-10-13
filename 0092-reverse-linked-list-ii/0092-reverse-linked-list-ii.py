e93bfcf81cdd04a2cf8fec58edbdb302e1c7f5f3
Raghav Dabra <66009953+RaghavDabra@users.noreply.github.com>
2022-10-13 15:59:55 +0530

    Time: 78 ms (5.01%), Space: 14 MB (52.02%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reverseBetween(self, head, m, n):
+        """
+        :type head: ListNode
+        :type m: int
+        :type n: int
+        :rtype: ListNode
+        """
+        if not head or m == n: return head
+        p = dummy = ListNode(None)
+        dummy.next = head
+        for i in range(m-1): p = p.next
+        tail = p.next
+
+        for i in range(n-m):
+            tmp = p.next                  # a)
+            p.next = tail.next            # b)
+            tail.next = tail.next.next    # c)
+            p.next.next = tmp             # d)
+        return dummy.next

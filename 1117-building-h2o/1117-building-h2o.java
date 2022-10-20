514f610d6e711974128001f53659ba03820f838a
Raghav Dabra <66009953+RaghavDabra@users.noreply.github.com>
2022-10-20 15:39:43 +0530

    Time: 32 ms (65.40%), Space: 46.1 MB (27.13%) - LeetHub

---
+++ 1117-building-h2o/1117-building-h2o.java
@@ -0,0 +1,39 @@
+import java.util.concurrent.*;
+
+class H2O {
+    
+    private final CyclicBarrier barrier = new CyclicBarrier(3);
+    private final Semaphore hSem = new Semaphore(2);
+    private final Semaphore oSem = new Semaphore(1);
+
+    public H2O() {
+        
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+		try {
+            hSem.acquire();
+            barrier.await();
+             // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+            releaseHydrogen.run();
+        } catch(Exception ignore) {
+            
+        } finally {
+            hSem.release();
+        }
+
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        try {
+            oSem.acquire();
+            barrier.await();
+            // releaseOxygen.run() outputs "O". Do not change or remove this line.
+            releaseOxygen.run();
+        } catch(Exception ignore) {
+            
+        } finally {
+            oSem.release();
+        }
+    }
+}

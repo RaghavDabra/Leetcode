74cdb30393d8e30804f8f890cd4d9f6ab2073293
Raghav Dabra <66009953+RaghavDabra@users.noreply.github.com>
2022-11-02 15:32:53 +0530

    Time: 589 ms (98.54%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,7 +1,1 @@
-SELECT
-    user_id,
-    MAX(time_stamp) AS 'last_stamp'
-FROM Logins
-WHERE YEAR(time_stamp) = 2020
-GROUP BY user_id
-;
+select user_id, max(time_stamp) as last_stamp from Logins where time_stamp like '%2020%' group by user_id
